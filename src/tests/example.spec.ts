ee89e1ee9f2ea632b72bb127185f3e19b36a8603
karwa <karwatmateusz07@gmail.com>
2024-05-18 18:40:31 +0200

    test

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test"
+import { expect, test } from "@playwright/test"
 import { forTest } from "@utils/sample.util"
 
 test("has title", async ({ page }) => {
@@ -6,24 +6,24 @@ test("has title", async ({ page }) => {
   await page.waitForTimeout(5000)
 })
 
-// test("failing test", async ({ page }) => {
-//   expect(1).toEqual(1)
-//   await someAsyncFunction("1", 2)
-//   await page.waitForTimeout(5000)
-// })
+test("failing test", async ({ page }) => {
+  expect(1).toEqual(1)
+  await someAsyncFunction("1", 2)
+  await page.waitForTimeout(5000)
+})
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/")
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/")
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click()
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click()
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible()
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible()
 
-//   await page.waitForTimeout(5000)
-// })
+  await page.waitForTimeout(5000)
+})
 
-// async function someAsyncFunction(name: string, id: number) {
-//   console.log(name, id)
-// }
+async function someAsyncFunction(name: string, id: number) {
+  console.log(name, id)
+}
