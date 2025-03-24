ab341e5de4e710db1f2fed4ce8e50b995b3a9d66
karwa <karwatmateusz07@gmail.com>
2025-03-24 12:38:37 +0100

    fsa

--- src/tests/example.spec.ts
+++ src/tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { expect, request, test } from "@playwright/test";
-import { apiFetch, apiGet, apiPost, apiPut, apiDelete, apiPatch, apiHead } from 'pw-api-plugin';
+// import { apiFetch, apiGet, apiPost, apiPut, apiDelete, apiPatch, apiHead } from 'pw-api-plugin';
 // import { secret } from "@setup/getAwsData"
 // import { configEnv } from "@setup/config"
 // import { forTest } from "@utils/sample.util"
@@ -9,55 +9,55 @@ import { apiFetch, apiGet, apiPost, apiPut, apiDelete, apiPatch, apiHead } from 
 //   await page.waitForTimeout(5000)
 // })
 
-test("TESTING COMMIT", { tag: "@qa" }, async ({ page }) => {
-  expect(1).toEqual(1);
-  console.log("TESTING COMMIT QA ENV ");
-  await page.waitForTimeout(100);
-  // console.log(await secret())
-});
+// test("TESTING COMMIT", { tag: "@qa" }, async ({ page }) => {
+//   expect(1).toEqual(1);
+//   console.log("TESTING COMMIT QA ENV ");
+//   await page.waitForTimeout(100);
+//   // console.log(await secret())
+// });
 
-test('apiFetch', async ({request, page }) => { 
-        const responsePut = await apiPut({ request, page }, 'https://jsonplaceholder.typicode.com/posts/1', {
-            body: {
-                id: 1,
-                title: 'foo',
-                body: 'bar',
-                userId: 1,
-            },
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-        expect(responsePut.ok()).toBeTruthy();
-        const responseBodyPut = await responsePut.json();
-        expect(responseBodyPut).toHaveProperty('id', 1);
-})
+// test('apiFetch', async ({request, page }) => { 
+//         const responsePut = await apiPut({ request, page }, 'https://jsonplaceholder.typicode.com/posts/1', {
+//             body: {
+//                 id: 1,
+//                 title: 'foo',
+//                 body: 'bar',
+//                 userId: 1,
+//             },
+//             headers: {
+//                 'Content-type': 'application/json; charset=UTF-8',
+//             },
+//         });
+//         expect(responsePut.ok()).toBeTruthy();
+//         const responseBodyPut = await responsePut.json();
+//         expect(responseBodyPut).toHaveProperty('id', 1);
+// })
 
 
 test.only('apiFetchNoCustom', async ({ request, page }, TestConfig) => {
   console.log(process.env.ENVVAR)
   console.log(typeof process.env.ENVVAR)
-  await page.goto('https://playwright.dev/');
-  const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
-  expect(response.ok()).toBeTruthy();
-  const responseBody = await response.json();
-  expect(responseBody).toHaveProperty('id', 1);
-  await expect(page.locator('.hero hero--primary heroBanner_UJJx')).toHaveScreenshot('responseBody.png');
+  // await page.goto('https://playwright.dev/');
+  // const response = await request.get('https://jsonplaceholder.typicode.com/posts/1');
+  // expect(response.ok()).toBeTruthy();
+  // const responseBody = await response.json();
+  // expect(responseBody).toHaveProperty('id', 1);
+  // await expect(page.locator('.hero hero--primary heroBanner_UJJx')).toHaveScreenshot('responseBody.png');
 })
 
-test("failing test", { tag: "@test" }, async ({ page }) => {
-  console.log("TEST ENV 1");
-  // console.log(configEnv)
-  // console.log(process.env)
-  // console.log(configEnv.ENV)
-  // console.log(process.env.ENV)
-  // console.log(process.env._test_beta_acc)
-  // console.log(process.env.TEST_BETA_ACC)
-  expect(1).toEqual(1);
-  await someAsyncFunction("1", 2);
-  expect(1).toEqual(2);
-  await page.waitForTimeout(5000);
-});
+// test("failing test", { tag: "@test" }, async ({ page }) => {
+//   console.log("TEST ENV 1");
+//   // console.log(configEnv)
+//   // console.log(process.env)
+//   // console.log(configEnv.ENV)
+//   // console.log(process.env.ENV)
+//   // console.log(process.env._test_beta_acc)
+//   // console.log(process.env.TEST_BETA_ACC)
+//   expect(1).toEqual(1);
+//   await someAsyncFunction("1", 2);
+//   expect(1).toEqual(2);
+//   await page.waitForTimeout(5000);
+// });
 
 // test("get started link", async ({ page }) => {
 //   await page.goto("https://playwright.dev/")
@@ -71,6 +71,6 @@ test("failing test", { tag: "@test" }, async ({ page }) => {
 //   await page.waitForTimeout(5000)
 // })
 
-async function someAsyncFunction(name: string, id: number) {
-  console.log(name, id);
-}
+// async function someAsyncFunction(name: string, id: number) {
+//   console.log(name, id);
+// }
