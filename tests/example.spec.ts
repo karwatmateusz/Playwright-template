c038e81481c7a2b2c96eeb8d047c1025f122f394
karwa <karwatmateusz07@gmail.com>
2025-03-30 23:48:36 +0200

    vcxz

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
-import test from "@playwright/test"
+import test, { expect } from "@playwright/test"
 
 test("has title", async ({ page }) => {
   console.log('TEST FOR RUNNING WORKFLOW')
-  await page.waitForTimeout(1000)
+  await page.waitForTimeout(5000)
+  expect(1).toBe(2)
 })
