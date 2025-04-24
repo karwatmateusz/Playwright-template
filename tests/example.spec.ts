904082b2f60324c635bf646071ee8fdc7e69d2fd
karwa <karwatmateusz07@gmail.com>
2025-04-24 09:17:57 +0200

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import test, { expect } from "@playwright/test"
 test("has title", async ({ page }) => {
   console.log('TEST FOR RUNNING WORKFLOW')
   await page.waitForTimeout(5000)
-  expect(1).toBe(2)
+  expect(1).toBe(1)
 })
