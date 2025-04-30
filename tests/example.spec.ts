33d95cc1c066182c9303d184862f4189960d2901
karwa <karwatmateusz07@gmail.com>
2025-04-30 18:11:44 +0200

    asd

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,6 @@ import test, { expect } from "@playwright/test"
 test("has title", async ({ page }) => {
   console.log('TEST FOR RUNNING WORKFLOW')
   await page.waitForTimeout(5000)
+  console.log('RUNNING TESTS IN PW REPO')
   expect(1).toBe(1)
 })
