56feebf037ffe6cea9d54a8d86409a6d2e40622f
karwa <karwatmateusz07@gmail.com>
2025-05-05 08:10:55 +0200

    testsecrets

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,8 @@ import test, { expect } from "@playwright/test"
 
 test("has title", async ({ page }) => {
   console.log('TEST FOR RUNNING WORKFLOW')
+  console.log('GITHUB_ENVSEC:', process.env.ENVSEC);
+  console.log('GITHUB_ENV_SECRET:', process.env.ENV_SECRET);
   await page.waitForTimeout(5000)
   console.log('RUNNING TESTS IN PW REPO')
   expect(1).toBe(1)
